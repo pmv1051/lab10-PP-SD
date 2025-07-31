e00a54d5b129cf12ba37ff08edf3fcb0e856ed4a
Samuel Deegan <samueldeegan@Samuels-MacBook-Pro.local>
2025-07-30 22:00:20 -0400

    part 2 p2 commit

--- test_calculator.py
+++ test_calculator.py
@@ -3,11 +3,11 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(1,2),3)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(2,1),1)
     # ##########################
 
     ######## Partner 1
@@ -19,19 +19,18 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0,5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(2,2),1)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(2, 0)
+        with self.assertRaises(ValueError):
+            log(2, -1)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
